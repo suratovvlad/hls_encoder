10c7d2f1e1604a2af0c1c58739aa6a4afa080158
Vladislav Suratov <suratovvlad@gmail.com>
2021-09-17 00:58:34 +0300

    Do not use raw video packet

--- src/hls_encoder.h
+++ src/hls_encoder.h
@@ -1005,14 +1005,11 @@ public:
 
             m_hls_stream_context->write_header();
 
-            frame video_input_frame;
-            packet video_input_packet;
+            auto video_input_frame = std::make_unique< frame >();
+            auto video_input_packet = std::make_unique< packet >();
 
             auto audio_input_frame = std::make_unique< frame >();
             auto audio_input_packet = std::make_unique< packet >();
-
-            const auto& _format_context_1 = m_video_src_input_context->get_stream_info()->get_format_context();
-            const auto& _format_context_2 = m_audio_src_input_context->get_stream_info()->get_format_context();
 
             std::vector< std::unique_ptr< frame > > _scaling_frames;
             _scaling_frames.reserve( m_scaling_options.size() );
@@ -1020,28 +1017,25 @@ public:
             {
                 _scaling_frames.emplace_back( std::make_unique< frame >() );
             }
-//        frame scaled_frame_1;
-//        frame scaled_frame_2;
-//        frame scaled_frame_3;
 
             bool no_video = false;
             bool no_audio = false;
             while( true )
             {
-                if( av_read_frame( _format_context_1.get(), video_input_packet.get() ) >= 0 )
+                if( const auto& _format_context_1 = m_video_src_input_context->get_stream_info()->get_format_context();
+                    av_read_frame( _format_context_1.get(), video_input_packet->get() ) >= 0 )
                 {
-                    if( _format_context_1.get_stream_by_idx( video_input_packet.get()->stream_index )->codecpar->codec_type == AVMEDIA_TYPE_VIDEO )
+                    if( _format_context_1.get_stream_by_idx( video_input_packet->get()->stream_index )->codecpar->codec_type == AVMEDIA_TYPE_VIDEO )
                     {
-//                    transcode_video(_input_stream_context_1, _output_stream_context_1, video_input_packet.get(), video_input_frame_1.get() );
-//                    transcode_video(_input_stream_context_1, _output_stream_context_2, video_input_packet.get(), video_input_frame_2.get() );
-                        transcode_video( video_input_packet.get(), video_input_frame.get(), _scaling_frames );
+                        transcode_video( video_input_packet, video_input_frame, _scaling_frames );
                     }
                 }
                 else
                 {
                     no_video = true;
                 }
-                if( av_read_frame( _format_context_2.get(), audio_input_packet->get() ) >= 0 )
+                if( const auto& _format_context_2 = m_audio_src_input_context->get_stream_info()->get_format_context();
+                    av_read_frame( _format_context_2.get(), audio_input_packet->get() ) >= 0 )
                 {
                     if( _format_context_2.get_stream_by_idx( audio_input_packet->get()->stream_index )->codecpar->codec_type == AVMEDIA_TYPE_AUDIO )
                     {
@@ -1058,7 +1052,6 @@ public:
                 }
             }
 
-
             m_hls_stream_context->write_trailer();
 
         }
@@ -1072,13 +1065,16 @@ public:
 
 public:
 
-    int encode_video( const std::unique_ptr< output_stream>& _video_stream, AVFrame *input_frame, int _stream_index ) {
-        if (input_frame) input_frame->pict_type = AV_PICTURE_TYPE_NONE;
+    int encode_video( const std::unique_ptr< output_stream>& _video_stream, const std::unique_ptr< frame >& input_frame, int _stream_index ) {
 
-        AVPacket *output_packet = av_packet_alloc();
-        av_init_packet(output_packet);
+        if (input_frame)
+            input_frame->get()->pict_type = AV_PICTURE_TYPE_NONE;
 
-        if (!output_packet) { throw std::runtime_error("could not allocate memory for output packet"); }
+        packet output_packet;
+
+        if ( !output_packet.get() ) {
+            throw std::runtime_error("could not allocate memory for output packet");
+        }
 
         const auto& _decoder = m_video_src_input_context->get_decoder();
         const auto& _stream_info = m_hls_stream_context->get_stream_info();
@@ -1089,31 +1085,30 @@ public:
         auto _encoder_video_stream = _video_stream->get_av_stream();
 
 
-        int response = avcodec_send_frame( _encoder_video_codec_context, input_frame);
+        int response = avcodec_send_frame( _encoder_video_codec_context, input_frame->get() );
 
         while (response >= 0) {
-            response = avcodec_receive_packet(_encoder_video_codec_context, output_packet);
+            response = avcodec_receive_packet( _encoder_video_codec_context, output_packet.get() );
             if (response == AVERROR(EAGAIN) || response == AVERROR_EOF) {
                 break;
             } else if (response < 0) {
                 throw std::runtime_error("Error while receiving packet from encoder: [" + helpers::error2string(response) + "]");
             }
 
-            output_packet->stream_index = _stream_index;
-            output_packet->duration = _encoder_video_stream->time_base.den / _encoder_video_stream->time_base.num / _decoder_video_stream->avg_frame_rate.num * _decoder_video_stream->avg_frame_rate.den;
+            output_packet.get()->stream_index = _stream_index;
+            output_packet.get()->duration = _encoder_video_stream->time_base.den / _encoder_video_stream->time_base.num / _decoder_video_stream->avg_frame_rate.num * _decoder_video_stream->avg_frame_rate.den;
 
-            av_packet_rescale_ts(output_packet, _decoder_video_stream->time_base, _encoder_video_stream->time_base);
-            response = av_interleaved_write_frame( _stream_info->get_format_context().get(), output_packet );
+            av_packet_rescale_ts(output_packet.get(), _decoder_video_stream->time_base, _encoder_video_stream->time_base);
+            response = av_interleaved_write_frame( _stream_info->get_format_context().get(), output_packet.get() );
             if (response != 0 && response != AVERROR(EINVAL)) {
                 throw std::runtime_error("Error while writing packet: [" + helpers::error2string(response) + "]");
             }
         }
-        av_packet_unref(output_packet);
-        av_packet_free(&output_packet);
+        av_packet_unref( output_packet.get() );
         return 0;
     }
 
-    int transcode_video( AVPacket *input_packet, AVFrame* input_frame,
+    int transcode_video( const std::unique_ptr< packet >& input_packet, const std::unique_ptr< frame >& input_frame,
                          const std::vector< std::unique_ptr< frame > >& _scaling_frames )
     {
         const auto& _decoder = m_video_src_input_context->get_decoder();
@@ -1121,49 +1116,30 @@ public:
 
         const auto& _encoder = m_hls_stream_context->get_encoder();
 
-        int response = avcodec_send_packet(_decoder_video_codec_context, input_packet);
+        int response = avcodec_send_packet( _decoder_video_codec_context, input_packet->get() );
         if (response < 0){
             throw std::runtime_error("Error while sending packet to decoder: [" + helpers::error2string(response) + "]");
         }
 
         while (response >= 0) {
-            response = avcodec_receive_frame(_decoder_video_codec_context, input_frame);
+            response = avcodec_receive_frame( _decoder_video_codec_context, input_frame->get() );
             if (response == AVERROR(EAGAIN) || response == AVERROR_EOF) {
                 break;
             } else if (response < 0 && response != AVERROR(EINVAL) ) {
                 throw std::runtime_error("Error while receiving frame from decoder: [" + helpers::error2string(response) + "]");
             }
 
-
             const int stream_index = _decoder->get_video_stream()->get_stream_index();
             if (response >= 0) {
-
-//            std::vector< AVFrame* > _scaled_frames;
-//            _scaled_frames.push_back( scaled_frame_1 );
-//            _scaled_frames.push_back( scaled_frame_2 );
-//            _scaled_frames.push_back( scaled_frame_3 );
                 for( hls_stream_encoder::stream_index _idx = 0; _idx < _encoder->get_streams_count(); ++_idx )
                 {
                     auto _scaling_frame = _scaling_frames.at( _idx )->get();
-                    m_hls_stream_context->get_video_scaler_by_index( _idx )->scale( input_frame, _scaling_frame );
-                    encode_video( _encoder->get_video_stream( _idx ), _scaling_frame, stream_index + _idx );
+                    m_hls_stream_context->get_video_scaler_by_index( _idx )->scale( input_frame->get(), _scaling_frame );
+                    encode_video( _encoder->get_video_stream( _idx ), _scaling_frames.at( _idx ), stream_index + _idx );
                 }
-
-//            {
-//                _encoder_context.get_video_scaler_by_index( 0 )->scale(input_frame, scaled_frame_1);
-//                encode_video_hls(_decoder_context, _encoder->get_video_stream( 0 ), _encoder_context.get_stream_info(), scaled_frame_1, stream_index);
-//            }
-//            {
-//                _encoder_context.get_video_scaler_by_index( 1 )->scale(input_frame, scaled_frame_2);
-//                encode_video_hls(_decoder_context,  _encoder->get_video_stream( 1 ), _encoder_context.get_stream_info(), scaled_frame_2, stream_index + 1);
-//            }
-//            {
-//                _encoder_context.get_video_scaler_by_index( 2 )->scale(input_frame, scaled_frame_3);
-//                encode_video_hls(_decoder_context,  _encoder->get_video_stream( 2 ), _encoder_context.get_stream_info(), scaled_frame_3, stream_index + 2);
-//            }
             }
 
-            av_frame_unref(input_frame);
+            av_frame_unref( input_frame->get() );
         }
 
         return 0;
